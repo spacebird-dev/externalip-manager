a0a52170eacb686e8e3e14c4b973e11e9b613142
Max Hösel <git@maxhoesel.de>
2026-05-01 00:32:06 +0200

    dnsHostname: use system resolvers

--- crates/manager/src/external_ip_source/solvers/dns_hostname.rs
+++ crates/manager/src/external_ip_source/solvers/dns_hostname.rs
@@ -1,8 +1,6 @@
-use std::net::IpAddr;
-
 use async_trait::async_trait;
-use hickory_resolver::{Resolver, config::ResolverConfig, name_server::TokioConnectionProvider};
-use itertools::Itertools;
+use hickory_resolver::{Resolver, net::runtime::TokioRuntimeProvider};
+
 use k8s_openapi::api::core::v1::Service;
 use tracing::instrument;
 
@@ -13,18 +11,17 @@ use super::Solver;
 #[derive(Debug)]
 pub struct DnsHostname {
     host: String,
-    resolver: Resolver<TokioConnectionProvider>,
+    resolver: Resolver<TokioRuntimeProvider>,
 }
 
 impl DnsHostname {
     pub fn new(host: String) -> DnsHostname {
         DnsHostname {
             host,
-            resolver: Resolver::builder_with_config(
-                ResolverConfig::default(),
-                TokioConnectionProvider::default(),
-            )
-            .build(),
+            resolver: Resolver::builder_tokio()
+                .expect("could not build DNS resolver")
+                .build()
+                .expect("could not build the resolver"),
         }
     }
 }
@@ -38,27 +35,18 @@ impl Solver for DnsHostname {
         _: &Service,
         _: &SolverRegistry,
     ) -> Result<Vec<std::net::IpAddr>, SolverError> {
-        match kind {
-            external_ip_source::AddressKind::IPv4 => Ok(self
-                .resolver
-                .ipv4_lookup(self.host.clone())
-                .await
-                .map_err(|e| SolverError {
-                    reason: e.to_string(),
-                })?
-                .iter()
-                .map(|a| IpAddr::V4(a.0))
-                .collect_vec()),
-            external_ip_source::AddressKind::IPv6 => Ok(self
-                .resolver
-                .ipv6_lookup(self.host.clone())
-                .await
-                .map_err(|e| SolverError {
-                    reason: e.to_string(),
-                })?
-                .iter()
-                .map(|a| IpAddr::V6(a.0))
-                .collect_vec()),
-        }
+        Ok(self
+            .resolver
+            .lookup_ip(self.host.clone())
+            .await
+            .map_err(|e| SolverError {
+                reason: e.to_string(),
+            })?
+            .iter()
+            .filter(|addr| match kind {
+                external_ip_source::AddressKind::IPv4 => addr.is_ipv4(),
+                external_ip_source::AddressKind::IPv6 => addr.is_ipv6(),
+            })
+            .collect())
     }
 }
